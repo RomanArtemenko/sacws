0d620f7547cd0fce86789733a8af42b2e0703a79
RomanArtemenko <RGArtemenko@gmail.com>
2022-02-15 01:01:52 +0200

    update rabbit_consumer

--- rabbit_consumer.py
+++ rabbit_consumer.py
@@ -1,9 +1,14 @@
+import json
+import hashlib
 import logging
 import sys
 from logging import StreamHandler, Formatter
 
 import asyncio
+import asyncpg
+import aio_pika
 from aio_pika import connect, Message
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -20,15 +25,34 @@ RABBITMQ_VHOST = '/'
 RABBITMQ_HOST = 'rabbit'
 RABBITMQ_PORT = 5672
 
-import asyncio
-import aio_pika
+# postgres
+POSTGRES_USER = 'geo'
+POSTGRES_PASSWORD = 'geo'
+POSTGRES_DB = 'geo'
+POSTGRES_HOST = 'postgres'
 
 
-async def process_message(message: aio_pika.IncomingMessage):
-    async with message.process():
-        print(message.body)
-        logger.info('got rabbit message: {}'.format(message.body))
-        await asyncio.sleep(1)
+async def save_to_db(shape_hash, shape):
+	conn = await asyncpg.connect(user=POSTGRES_USER, password=POSTGRES_PASSWORD,
+                                 database=POSTGRES_DB, host=POSTGRES_HOST)
+
+	res = await conn.fetch('''
+		INSERT INTO geo_data(hash, shape) 
+		VALUES($1, ST_GeomFromGeoJSON($2))''',
+		shape_hash, shape)
+
+	await conn.close()
+
+
+async def process_message(msg: aio_pika.IncomingMessage):
+    async with msg.process():
+        logger.info('got rabbit message: {}'.format(msg.body))
+        msg_json = json.loads(msg.body)
+        params = msg_json['params']
+        geometry_hash = hashlib.md5(repr(params['geometry']).encode()).hexdigest()
+        logger.info('msg_json: {}'.format(msg_json))
+        logger.info('geometry_hash: {}'.format(geometry_hash))
+        await save_to_db(geometry_hash, json.dumps(params['geometry']))
 
 
 async def main(loop):
@@ -63,7 +87,6 @@ if __name__ == "__main__":
     connection = loop.run_until_complete(main(loop))
 
     try:
-    	# logger.info('got kafka message: {}'.format(msg.value))
         logger.info(f'satrt consuming from host {RABBITMQ_HOST}:{RABBITMQ_PORT}')
         loop.run_forever()
     finally:
